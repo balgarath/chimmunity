e9733db98b5c7a360e8c73b93ba99b83ebf891ec
balgarath <rledge21@gmail.com>
2015-12-31 02:56:41 -0500

    initial commit #ralphwashere

---
+++ config/application_config.rb
@@ -0,0 +1,7 @@
+configatron.community_name = "Chimmunity"
+# See CE's application_config.rb to see all the other configuration options available
+configatron.app_host = "chimmunity.com"
+configatron.community_tagline = "Chi Community Online Social Network"
+configatron.meta_description = "Chi Community Online Social Network"
+configatron.require_captcha_on_signup = false
+
