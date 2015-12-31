f246f4a82563a46cd01c15570edee8676d40616a
balgarath <rledge21@gmail.com>
2015-12-31 16:02:11 +0000

    production tweaks

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.0.13'
 gem 'mysql2', '~> 0.3.18'
 
 gem 'aws-s3'
+gem 'omniauth-facebook'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
--- config/application_config.rb
+++ config/application_config.rb
@@ -4,4 +4,3 @@ configatron.app_host = "chimmunity.com"
 configatron.community_tagline = "Chi Community Online Social Network"
 configatron.meta_description = "Chi Community Online Social Network"
 configatron.require_captcha_on_signup = false
-
